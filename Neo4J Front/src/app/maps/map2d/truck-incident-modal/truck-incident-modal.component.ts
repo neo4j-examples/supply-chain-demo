2c9bf0cd6fcba4bb24164fcc365f784e628070fe
Javier Plaza <javier.plaza@bravent.net>
2022-01-04 13:06:06 +0100

    Call and galons

--- Neo4J Front/src/app/maps/map2d/truck-incident-modal/truck-incident-modal.component.ts
+++ Neo4J Front/src/app/maps/map2d/truck-incident-modal/truck-incident-modal.component.ts
@@ -12,7 +12,7 @@ import Chart from 'chart.js/auto';
 
 import { Incident } from '../../models/incident.model';
 import { Truck } from '../../models/truck.model';
-import { EChartsOption } from 'echarts'
+import { EChartsOption } from 'echarts';
 import * as echarts from 'echarts';
 @Component({
   selector: 'neo4j-truck-incident-modal',
@@ -43,25 +43,25 @@ export class TruckIncidentModalComponent implements OnInit, AfterViewInit {
           shadowColor: 'rgba(0,138,255,0.45)',
           shadowBlur: 10,
           shadowOffsetX: 2,
-          shadowOffsetY: 2
+          shadowOffsetY: 2,
         },
         progress: {
           show: true,
           roundCap: true,
-          width: 7
+          width: 7,
         },
         pointer: {
           icon: 'path://M2090.36389,615.30999 L2090.36389,615.30999 C2091.48372,615.30999 2092.40383,616.194028 2092.44859,617.312956 L2096.90698,728.755929 C2097.05155,732.369577 2094.2393,735.416212 2090.62566,735.56078 C2090.53845,735.564269 2090.45117,735.566014 2090.36389,735.566014 L2090.36389,735.566014 C2086.74736,735.566014 2083.81557,732.63423 2083.81557,729.017692 C2083.81557,728.930412 2083.81732,728.84314 2083.82081,728.755929 L2088.2792,617.312956 C2088.32396,616.194028 2089.24407,615.30999 2090.36389,615.30999 Z',
           length: '70%',
           width: 6,
-          offsetCenter: [0, '5%']
+          offsetCenter: [0, '5%'],
         },
         axisLine: {
           roundCap: true,
           lineStyle: {
             width: 7,
-            color: [[1, '#58D9F944']]
-          }
+            color: [[1, '#58D9F944']],
+          },
         },
         axisTick: {
           length: 4,
@@ -69,24 +69,24 @@ export class TruckIncidentModalComponent implements OnInit, AfterViewInit {
           splitNumber: 2,
           lineStyle: {
             width: 1,
-            color: '#55f9e2'
-          }
+            color: '#55f9e2',
+          },
         },
         splitLine: {
           length: 8,
           distance: 2,
           lineStyle: {
             width: 1,
-            color: '#55f9e2'
-          }
+            color: '#55f9e2',
+          },
         },
         axisLabel: {
           distance: 10,
           color: '#999',
-          fontSize: 9
+          fontSize: 9,
         },
         title: {
-          show: false
+          show: false,
         },
         detail: {
           width: '60%',
@@ -102,22 +102,22 @@ export class TruckIncidentModalComponent implements OnInit, AfterViewInit {
             value: {
               fontSize: 16,
               fontWeight: 'bolder',
-              color: '#777'
+              color: '#777',
             },
             unit: {
               fontSize: 10,
               color: '#999',
-              padding: [0, 0, -10, 5]
-            }
-          }
+              padding: [0, 0, -10, 5],
+            },
+          },
         },
         data: [
           {
-            value: 100
-          }
-        ]
-      }
-    ]
+            value: 100,
+          },
+        ],
+      },
+    ],
   };
   optionMPG = {
     series: [
@@ -125,7 +125,7 @@ export class TruckIncidentModalComponent implements OnInit, AfterViewInit {
         type: 'gauge',
         startAngle: 180,
         endAngle: 0,
-        min: 1,
+        min: 0,
         max: 120,
         splitNumber: 8,
         itemStyle: {
@@ -133,25 +133,25 @@ export class TruckIncidentModalComponent implements OnInit, AfterViewInit {
           shadowColor: 'rgba(0,138,255,0.45)',
           shadowBlur: 10,
           shadowOffsetX: 2,
-          shadowOffsetY: 2
+          shadowOffsetY: 2,
         },
         progress: {
           show: true,
           roundCap: true,
-          width: 7
+          width: 7,
         },
         pointer: {
           icon: 'path://M2090.36389,615.30999 L2090.36389,615.30999 C2091.48372,615.30999 2092.40383,616.194028 2092.44859,617.312956 L2096.90698,728.755929 C2097.05155,732.369577 2094.2393,735.416212 2090.62566,735.56078 C2090.53845,735.564269 2090.45117,735.566014 2090.36389,735.566014 L2090.36389,735.566014 C2086.74736,735.566014 2083.81557,732.63423 2083.81557,729.017692 C2083.81557,728.930412 2083.81732,728.84314 2083.82081,728.755929 L2088.2792,617.312956 C2088.32396,616.194028 2089.24407,615.30999 2090.36389,615.30999 Z',
           length: '70%',
           width: 6,
-          offsetCenter: [0, '5%']
+          offsetCenter: [0, '5%'],
         },
         axisLine: {
           roundCap: true,
           lineStyle: {
             width: 7,
-            color: [[1, '#58D9F944']]
-          }
+            color: [[1, '#58D9F944']],
+          },
         },
         axisTick: {
           length: 4,
@@ -159,24 +159,24 @@ export class TruckIncidentModalComponent implements OnInit, AfterViewInit {
           splitNumber: 2,
           lineStyle: {
             width: 1,
-            color: '#55f9e2'
-          }
+            color: '#55f9e2',
+          },
         },
         splitLine: {
           length: 8,
           distance: 2,
           lineStyle: {
             width: 1,
-            color: '#55f9e2'
-          }
+            color: '#55f9e2',
+          },
         },
         axisLabel: {
           distance: 10,
           color: '#999',
-          fontSize: 9
+          fontSize: 9,
         },
         title: {
-          show: false
+          show: false,
         },
         detail: {
           width: '60%',
@@ -192,22 +192,22 @@ export class TruckIncidentModalComponent implements OnInit, AfterViewInit {
             value: {
               fontSize: 16,
               fontWeight: 'bolder',
-              color: '#777'
+              color: '#777',
             },
             unit: {
               fontSize: 10,
               color: '#999',
-              padding: [0, 0, -10, 5]
-            }
-          }
+              padding: [0, 0, -10, 5],
+            },
+          },
         },
         data: [
           {
-            value: 35
-          }
-        ]
-      }
-    ]
+            value: 35,
+          },
+        ],
+      },
+    ],
   };
   mph: void;
   mpg: void;
@@ -216,22 +216,18 @@ export class TruckIncidentModalComponent implements OnInit, AfterViewInit {
   ngOnInit(): void {}
 
   ngAfterViewInit(): void {
-   echarts.init(this.chart.nativeElement).setOption(this.option)
-   echarts.init(this.chartTwo.nativeElement).setOption(this.optionMPG)
- 
+    echarts.init(this.chart.nativeElement).setOption(this.option);
+    echarts.init(this.chartTwo.nativeElement).setOption(this.optionMPG);
+
     this.startRandom(this.chartTwo.nativeElement, 60, 30, 'optionMPG');
     this.startRandom(this.chart.nativeElement, 75, 110, 'option');
-
   }
-
-
 
   closeClicked(): void {
     this.closeModal.emit();
   }
 
-
-  private updateChart(chart: any, max:number, min: number, model: string) {
+  private updateChart(chart: any, max: number, min: number, model: string) {
     const rand = Math.floor(Math.random() * (max - min + 1) + min);
     (this as any)[model].series[0].data = [{ value: rand }];
     echarts.getInstanceByDom(chart)?.setOption((this as any)[model]);
